d39406b8aed75d5235eda706579c764f5753d396
chenaoh <joseulises.jg@gmail.com>
2024-09-20 18:53:28 -0600

    Se agrega el proyecto

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function redirection() {
+    window.location.href = 'pages/razerblade14.html';
+  }
+  
+  function redirection2() {
+    window.location.href = 'pages/huawei-matebook-x-pro.html';
+  }
+
+  function redirection3() {
+    window.location.href = 'pages/huawei-matebook-14-2024.html';
+  }
+
+  function redirection4() {
+    window.location.href = 'pages/ASUS TUF Gaming F15.html';
+  }
+
+  function redirection5() {
+    window.location.href = 'pages/Legion 5i 16.html';
+  }
